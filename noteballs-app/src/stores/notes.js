d6ddb09bf3b933db75e6c9f8ccfc71f3a81c9ecd
DeeJayJay <deejayjay@zoho.com>
2023-08-13 13:48:21 -0600

    Completes firebase and firestore setup.

--- noteballs-app/src/stores/notes.js
+++ noteballs-app/src/stores/notes.js
@@ -1,11 +1,12 @@
+import { firestore } from "../js/firebase";
 import { defineStore } from "pinia";
-import { useToast } from 'vue-toast-notification';
+import { useToast } from "vue-toast-notification";
 
 // Toast
 const $toast = useToast();
 const toastOptions = {
-  position: 'top',
-  duration: 3000,
+  position: "top",
+  duration: 3000
 };
 
 export const useNotesStore = defineStore("notes", {
@@ -14,7 +15,7 @@ export const useNotesStore = defineStore("notes", {
   }),
   actions: {
     addNote(newNoteContent) {
-      if (newNoteContent.trim() === '') {
+      if (newNoteContent.trim() === "") {
         return;
       }
 
@@ -23,27 +24,31 @@ export const useNotesStore = defineStore("notes", {
         content: newNoteContent.trim()
       });
 
-      $toast.success('The note has been added successfully!', toastOptions);
+      $toast.success("The note has been added successfully!", toastOptions);
     },
     deleteNoteById(id) {
-      this.notes = this.notes.filter(note => note.id !== id);
-      $toast.success('The note was deleted successfully!', toastOptions);
+      this.notes = this.notes.filter((note) => note.id !== id);
+      $toast.success("The note was deleted successfully!", toastOptions);
     },
     updateNoteById(id, newNoteContent) {
-      const note = this.notes.find(note => note.id === id);
+      const note = this.notes.find((note) => note.id === id);
       note.content = newNoteContent.trim();
 
-      $toast.success('The note has been updated successfully!', toastOptions);
+      $toast.success("The note has been updated successfully!", toastOptions);
     }
   },
   getters: {
     getNoteContent: (state) => {
       return (id) => {
-        const note = state.notes.find(note => note.id === id);
+        const note = state.notes.find((note) => note.id === id);
         return note.content;
-      }
+      };
     },
     totalNumberOfNotes: (state) => state.notes.length,
-    totalNoOfCharacters: (state) => state.notes.reduce((total, currentNote) => total + currentNote.content.length, 0)
+    totalNoOfCharacters: (state) =>
+      state.notes.reduce(
+        (total, currentNote) => total + currentNote.content.length,
+        0
+      )
   }
 });
